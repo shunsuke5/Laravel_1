8d20dd23dd94d96aa99648f9c3e58b4948b65c54
shunsuke5 <shunsuke1359@gmail.com>
2024-06-28 20:47:13 +0900

    20240628 20:47 Bootcamp start

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\Laravel1Controller;
+use App\Http\Controllers\ProfileController;
+use App\Models\Laravel_1;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::resource('Laravel_1s', Laravel1Controller::class)
+    ->only(['index', 'store'])
+    ->middleware(['auth', 'verified']);
+
+require __DIR__.'/auth.php';
