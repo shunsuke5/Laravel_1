9cb6365dceac0361f8ea3d2e1065772bdb237fbc
shunsuke5 <shunsuke1359@gmail.com>
2024-06-29 18:46:41 +0900

    20240629 18:46

--- app/Models/Laravel_1.php
+++ app/Models/Laravel_1.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Laravel_1 extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'message',
+    ];
 }
--- database/migrations/2024_06_28_080241_create_laravel_1s_table.php
+++ database/migrations/2024_06_28_080241_create_laravel_1s_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('laravel_1s', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('message');
             $table->timestamps();
         });
     }
